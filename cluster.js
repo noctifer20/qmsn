80f563caed7877b3af50a7b4fecd7b5fe3d386a6
pvo <pvo@qwe.ewq>
2019-06-03 22:49:15 +0400

    add new function in redis and add mysql connection

--- cluster.js
+++ cluster.js
@@ -5,6 +5,22 @@ const mysql = require('mysql2');
 const Redis1 = require('./lib/utils/redis')
 
 
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1111',
+  database : 'testdb'
+});
+//
+// connection.connect();
+//
+//
+// connection.query('SELECT * from users where id = 159 limit 500', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results);
+// });
+
+
 
 /*subscriber.on('message', (channel, message) => {
   console.log('got ', channel);
@@ -29,13 +45,37 @@ class Cluster {
   }
 
   initSubscriber(){
-    his.redisdb.subscriberon(this.subscriberOn, (channel, message) => {
+    this.redisdb.subscriberon(this.subscriberOn, (channel, message) => {
       console.log('got ', channel);
       message = JSON.parse(message);
-      console.log("constructor");
+      console.log("constructor", message.range , new Date().getHours(), new Date().getMinutes(), new Date().getMilliseconds())
+
+      if(channel == 'limit'){
+        console.log(message);
+
+    }
+
 
       // this.publishAndAdd(this.publishTO,{ id: message.id, range: 1000 } )
     });
+    this.redisdb.subscribe(this.subscribeTo);
+
+
+
+    this.redisdb.publishAndAdd('server_' + Math.random(),{ ['server_' + Math.random()]: false } );
+
+
+    // this.redisdb.publishAndAdd('taskEnd',{ limit: 1 } );
+
+
   }
+  nextRange(){
+    this.publishAndAdd('nextRange',  )
+  }
+
+
+
+
+
 }
  new Cluster();
